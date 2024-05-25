fd048c0e452164559d77b68c96f202fbae170fae
Fajardo-Marcial <Eduardo.Fajardo-marcial@hhs.gov>
2024-05-25 00:54:12 -0400

    new page

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return(render(request, 'main/index.html'))
+
+def new(request):
+    return(render(request, 'main/new.html'))
